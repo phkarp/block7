5dedf88ae315c5941b74473a5e4d8a878404f96b
Ivan Karpushkin <karpushkin_ioann@mail.ru>
2022-12-31 16:07:40 +0300

    Initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -15,31 +15,8 @@ let orderCallBtnBurger = document.querySelector('.order-call__btn-burger');
 let btnCall = document.querySelectorAll('.btn--call');
 let orderCall = document.querySelector('.order-call-body');
 
-let beforeFeedback = document.querySelector('.feedback');
-
 let leftMenu = document.querySelector('.body-left-menu');
 let body = document.querySelector('.body')
-
-
-document.onclick = function (e) {
-  console.log(1);
-  if (feedback.style.display !== 'none') {
-    console.log(2);
-    if (e.target.className === 'feedback-body') {
-      feedback.style.display = 'none';
-      console.log(3);
-    }
-  }
-
-  if (orderCall.style.display !== 'none') {
-    console.log(4);
-    if (e.target.className === 'order-call-body') {
-      orderCall.style.display = 'none';
-      console.log(5);
-    }
-  }
-
-}
 
 
 upperMenuBtnBurger.addEventListener('click', () => {
@@ -50,12 +27,30 @@ leftMenuBtnBurger.addEventListener('click', () => {
   leftMenu.style.display = 'none';
   body.style.overflow = 'unset';
 })
+
 for (let i = 0; i < btnChat.length; i++) {
   btnChat[i].addEventListener('click', () => {
     feedback.style.display = 'block';
     body.style.overflow = 'hidden';
+    document.onclick = function (e) {
+      console.log(1);
+
+      if (feedback.style.display !== 'none') {
+        console.log(2);
+        if (e.target.className === 'feedback-body') {
+          feedback.style.display = 'none';
+          console.log(3);
+        }
+      } else {
+        document.onclick = () => {
+            console.log(15);
+        }
+      }
+    }
   })
 }
+
+
 
 feedbackBtnBurger.addEventListener('click', () => {
   feedback.style.display = 'none';
@@ -65,6 +60,22 @@ for (let i = 0; i < btnChat.length; i++) {
   btnCall[i].addEventListener('click', () => {
     orderCall.style.display = 'block';
     body.style.overflow = 'hidden';
+
+    document.onclick = function (e) {
+      console.log(1);
+
+      if (orderCall.style.display !== 'none') {
+        console.log(4);
+        if (e.target.className === 'order-call-body') {
+          orderCall.style.display = 'none';
+          console.log(5);
+        }
+      } else {
+        document.onclick = () => {
+          console.log(15);
+        }
+      }
+    }
   })
 }
 
